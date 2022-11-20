9a6929011ad7bfeb932bfd007088a8b69b7e5fe4
Charls <cgalvis21_@hotmail.com>
2022-11-20 17:30:07 -0500

    update

--- LinkedList.py
+++ LinkedList.py
@@ -7,24 +7,23 @@ class LinkedList:
         self.ULT = None
 
     def AddNode(self):
+        """agrega la fila de la matriz a un nodo"""
         file = open('Matriz21x21.txt')
         while(True):
-            linea = file.readline
-            ln = linea.split(' ')
-            P = Nodo()
+            """leer linea por linea y agregar esos valores al nodo"""
+            linea = file.readline()
+            ln = linea.split(" ")
+            P = Nodo(",".join(ln))
             if (self.PTR == None):
                 self.PTR = P
                 self.ULT = P 
             else:
                 self.ULT.next = P
-                self.ULT = P             
+                self.ULT = P
 
-    def Recorrido(self):
-        P = self.PTR
-        while(P != None):
-            print(P.data, end="->")
-            P = P.next
-        print("None")
+            if not linea:
+                break
+        file.close()
     
     def __repr__(self):
         respuesta = ""
@@ -32,5 +31,5 @@ class LinkedList:
         while(P != None):
             respuesta = respuesta + str(P.data) + "->"
             P = P.next
-        respuesta = respuesta + "None"
+        respuesta = respuesta + "Final Matriz"
         return respuesta
---
+++ prueba.py
@@ -0,0 +1,6 @@
+from LinkedList import LinkedList
+lista = LinkedList()
+lista.AddNode()
+
+
+print(lista)
