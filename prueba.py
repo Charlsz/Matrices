ce8d319f9d8811265ff393a0368056ba55b41961
Estudiantes <valentina_pajaro@hotmail.com>
2022-11-21 15:45:57 -0500

    update

--- prueba.py
+++ prueba.py
@@ -2,5 +2,11 @@ from LinkedList import LinkedList
 lista = LinkedList()
 lista.AddNode()
 
+lista2 = LinkedList()
+lista2.AddNode2()
 
+print("La primera matriz es: ")
 print(lista)
+
+print("La segunda matriz es: ")
+print(lista2)
